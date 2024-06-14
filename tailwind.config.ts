07af06d172470e741096fb577fefa248082a69d1
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-06-14 09:46:40 -0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        bg_login_and_register: "#191919",
+        yellow_color: "#dec74e",
+        blue_btn: "#445599",
       },
     },
   },
