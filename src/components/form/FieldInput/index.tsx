489ea988f3006314f54f9967d1c9ddbdb340d81e
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-10-22 10:16:08 -0300

    feat: feature to show or hide password in input

--- src/components/form/FieldInput/index.tsx
+++ src/components/form/FieldInput/index.tsx
@@ -2,6 +2,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { FieldError, UseFormRegister } from "react-hook-form";
 import { ErrorFieldInput } from "../ErrorFieldInput";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useState } from "react";
 
 interface FieldInputProps {
   label: string;
@@ -24,18 +26,39 @@ export const FieldInput = ({
   type,
   register,
 }: FieldInputProps) => {
+  const [inputType, setInputType] = useState(type);
+
+  const handleInputType = () => {
+    setInputType((prev) => (prev === "password" ? "text" : "password"));
+  };
+
   return (
     <div className="space-y-1">
       <Label htmlFor={name} className="text-white font-medium text-lg">
         {label}
       </Label>
-      <Input
-        id={name}
-        placeholder={placeholder}
-        className="h-12"
-        type={type}
-        {...register(name)}
-      />
+      <div className="relative">
+        <Input
+          id={name}
+          placeholder={placeholder}
+          className="h-12"
+          type={inputType}
+          {...register(name)}
+        />
+
+        {name === "password" && (
+          <div
+            onClick={handleInputType}
+            className="cursor-pointer absolute right-3 top-2/4 -translate-y-2/4"
+          >
+            {inputType === "password" ? (
+              <EyeOffIcon className="h-5" />
+            ) : (
+              <EyeIcon className="h-5" />
+            )}
+          </div>
+        )}
+      </div>
       <ErrorFieldInput error={error?.message} />
     </div>
   );
